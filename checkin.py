09045cebe665a6cb1d84859901d2ce53775b876c
594233 <75619926+594233@users.noreply.github.com>
2023-08-09 14:00:05 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,40 +1,46 @@
 import requests,json,os
 
-# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = os.environ["SERVE"]
-# 填写server酱sckey,不开启server酱则不用填
-sckey = os.environ["SCKEY"]
-#'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
+# 推送开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+# sever = os.environ["SERVE"]
+sever = "off"
+# 填写pushplus的sckey,不开启推送则不用填
+# sckey = os.environ["SCKEY"]
+sckey = "off"
 # 填入glados账号对应cookie
-cookie = os.environ["COOKIE"]
-#'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
+# COOKIES = os.environ["COOKIES"]
+COOKIES = "__stripe_mid=9476cbd5-0c21-4a09-aacd-4fa78fcd80909d5a19; _gid=GA1.2.1350363516.1691398492; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc; koa:sess=eyJ1c2VySWQiOjM0MDUzNiwiX2V4cGlyZSI6MTcxNzQxOTU1NTE4MiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=QDCJSmV0qt8j4wKZNuLzKBvpBdo; _gat_gtag_UA_104464600_2=1; _ga=GA1.1.417899199.1666864336; _ga_CZFVKMNT9J=GS1.1.1691557120.34.1.1691560612.0.0.0"
+print(COOKIES)
+cookies=COOKIES.split('&&')
 
 
 
 def start():
-    print("begin to check in !!!")
-    print(cookie)
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-   # print(res)
-
-    if 'message' in checkin.text:
-        mess = checkin.json()['message']
+    origin = "https://glados.rocks"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
+    payload={
+        'token': 'glados.one'
+    }
+    for cookie in cookies:
+        checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+        state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    #--------------------------------------------------------------------------------------------------------#
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        #print(time)
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
-    else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        email = state.json()['data']['email']
+        if 'message' in checkin.text:
+            mess = checkin.json()['message']
+            if sever == 'on':
+                requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+mess+'&content='+email+' 剩余'+time+'天')
+        else:
+            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'更新cookie')
+     #--------------------------------------------------------------------------------------------------------#
+
 
 def main_handler(event, context):
   return start()
 
 if __name__ == '__main__':
     start()
-
-    
